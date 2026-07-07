5b8be859341c066a13c98d7591657144a3262491
Jiyin Zhang <zhangjiyin2000@gmail.com>
2026-07-07 14:22:15 -0700

    created proj skeleton

---
+++ src/main/java/com/bfs/projectstory/restapipracticefive/RestApiPracticeFiveApplication.java
@@ -0,0 +1,13 @@
+package com.bfs.projectstory.restapipracticefive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPracticeFiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiPracticeFiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bfs/projectstory/restapipracticefive/RestApiPracticeFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfs.projectstory.restapipracticefive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPracticeFiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
